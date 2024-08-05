13b9a48bfde637c14805d50512e537a92d795478
RustyGod23 <140146471+RustyGod23@users.noreply.github.com>
2024-08-05 21:56:17 +0300

    Add files via upload

---
+++ string.py
@@ -0,0 +1,6 @@
+example = "Сияние"  # 1 пункт
+print(example[0])  # 2 пункт
+print(example[-1])  # 3 пункт
+print(example[3:])  # 4 пункт
+print(example[::-1])  # 5 пункт
+print(example[1::2])  # 6 пункт

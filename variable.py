cbc3b6e39190a5a504c0486906150623405e4449
RustyGod23 <140146471+RustyGod23@users.noreply.github.com>
2024-08-06 22:33:41 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,7 @@
+number_of_compledet_homework = 12
+number_of_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_spent / number_of_compledet_homework
+print('Курс:', course_name,
+      ', затрачено часов:', number_of_compledet_homework,
+      ', Среднее время выполнения:', time_for_one_task)
